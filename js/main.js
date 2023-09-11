de555aa32329b1f6163931d7e9a93a18b1d3f7ed
Antonella Polech <polechantonella1998@gmail.com>
2023-09-11 09:13:37 -0300

    Funciones

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 //* El simulador interactivo va a indicar el costo total de servicios seleccionados por el usuario. En mi caso utilizo un proyecto de desarrollo web de una pagina de estudio contable que contiene una seccion de "servicios" que voy a utilizar para este proyecto de JS. 
 //---------------------INGRESO DEL USUARIO---------------------------
-function ingresarUsuario (){
-    let usuario = prompt ("Ingrese usuario")
-    alert ("El usuario ingresar es " + usuario)
-    console.log ("¡Hola " + usuario + "!")
+function ingresarUsuario() {
+    let usuario = prompt("Ingrese usuario")
+    alert("El usuario ingresar es " + usuario)
+    console.log("¡Hola " + usuario + "!")
 }
 
 ingresarUsuario();
@@ -13,17 +13,17 @@ ingresarUsuario();
 //----------------------SELECCION DE SERVICIOS y ARMADO DE CARRITO-------------------------------------
 let servicio1 = Number(1000);
 let servicio2 = Number(2000);
-let variable1= Number(servicio1+servicio2);
+let variable1 = Number(servicio1 + servicio2);
 
 let seleccionServicio = prompt("Seleccione los servicios a cotizar: servicio1; servicio2 ó variable1").toLocaleLowerCase();
 
-if(seleccionServicio == "servicio1"){
+if (seleccionServicio == "servicio1") {
     console.log("Cotización del servicio = " + servicio1);
     seleccionServicio = Number(servicio1);
-} else if(seleccionServicio == "servicio2"){
+} else if (seleccionServicio == "servicio2") {
     console.log("Cotización del servicio = " + servicio2);
     seleccionServicio = Number(servicio2);
-} else if(seleccionServicio == "variable1"){
+} else if (seleccionServicio == "variable1") {
     console.log("Cotización del servicio = " + variable1);
     seleccionServicio = Number(variable1);
 } else {
@@ -31,17 +31,34 @@ if(seleccionServicio == "servicio1"){
 }
 
 
+const cantidadServicios = Number(prompt("Indique la cantidad por servicio"));
+console.log("Se agregaron " + cantidadServicios + " a la cotización")
+
+
+
 
 //--------------------------------COSTO TOTAL DE COTIZACIÓN--------------------------------
-const cantidadServicios = Number(prompt("Indique la cantidad por servicio"));
-
 let costoServicio = Number(seleccionServicio);
 
-for (let i = 0; i <= cantidadServicios; i++);{
-    if(cantidadServicios > 0){
+
+for (let i = 0; i <= cantidadServicios; i++); {
+    if (cantidadServicios > 0) {
         let costoTotal = seleccionServicio * cantidadServicios;
         console.log("Servicio:" + seleccionServicio + " Cantidad:" + cantidadServicios + " Costo total= " + costoTotal);
     }
+    else {
+        console.log("No se agregaron servicios a la cotización");
+    }
+}
+
+
+/*FINALIZAR COTIZACIÓN*/
+let costoTotal = seleccionServicio * cantidadServicios;
+
+if (finalizarCotizacion = true) {
+    console.log("El costo final cotizado es " + costoTotal);
+} else{
+    console.log ("Continuar comprando")
 }
 
 //--------------------------------METODOS DE PAGO--------------------------------
